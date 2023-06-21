5ecc97ad2c5a676ac456effdf3b4d8d74eec6e37
Andrew Ross <134294344+andycamguy@users.noreply.github.com>
2023-06-21 19:41:47 -0400

    scrapped it and start anew

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
-let CurrentPlayer = 'X'
- //define the UI and create the game board brain of 
-    let gameboard = {
-      tile_one: {
-        display: document.getElementById("tile1"),
-        input: function() {
-          this.symbol = 'X';
-          this.display.innerText = this.symbol;
-          console.log('hello');
-        },
-        symbol: ''
-      }
-    };
-  
-    gameboard.tile_one.display.addEventListener('click', gameboard.tile_one.input);
+// let's start over again
+// BREAK THE PROBLEM INTO MULTIPLE PIECES YOU DUMB F*** STOP TRYING TO MAKE IT ONE BIG PIECE
+//EVEN THOUGH IT IS A BIG PIECE, BREAK IT THE F*** DOWN
+let CurrentPlayer = 'X';
+let boardArray = [1,2,3,4,5,6,7,8,9] // create the board as an array
+
+let outputElement = document.getElementById("output");// access the div I want to display
+
+boardArray.forEach((item) => {
+    outputElement.innerHTML += item + "<br>"; // append the item to the innerHTML
   });
- 
-  
-  
-  
