3660b835fbe2080fef1675c72186bcac61c1a99e
Andrew Ross <134294344+andycamguy@users.noreply.github.com>
2023-06-21 17:05:08 -0400

    setting up tile one and learn about interactivity

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-
+let CurrentPlayer = 'X'
+ //define the UI and create the game board brain of 
+    let gameboard = {
+      tile_one: {
+        display: document.getElementById("tile1"),
+        input: function() {
+          this.symbol = 'X';
+          this.display.innerText = this.symbol;
+          console.log('hello');
+        },
+        symbol: ''
+      }
+    };
+  
+    gameboard.tile_one.display.addEventListener('click', gameboard.tile_one.input);
+  });
+ 
+  
+  
+  
