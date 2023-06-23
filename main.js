85cf2ccd71d972796f7efce0a6269e926e7b08d8
Andrew Ross <134294344+andycamguy@users.noreply.github.com>
2023-06-23 08:08:54 -0400

    it basically works. few kinks

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// let's start over again
-// BREAK THE PROBLEM INTO MULTIPLE PIECES YOU DUMB F*** STOP TRYING TO MAKE IT ONE BIG PIECE
-//EVEN THOUGH IT IS A BIG PIECE, BREAK IT THE F*** DOWN
 let boardArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "]; // create the board as an array
-CurrentPlayer = "X";
-let game = true;
+let currentPlayer = "X";
+let playerXscore = 0;
+let playerOscore = 0;
+let gameActive = true; // flag variable to track the game state
+
 const winningCombinations = [
   [0, 1, 2], // Horizontal combinations
   [3, 4, 5],
@@ -14,11 +14,9 @@ const winningCombinations = [
   [0, 4, 8], // Diagonal combinations
   [2, 4, 6]
 ];
-let playerXinputs = [];
-let playerOinputs = [];
 
 function boardLayout() {
-  let outputElement = document.getElementById("output"); // access the div I want to display
+  let outputElement = document.getElementById("output"); // access the div to display the grid
 
   // Create the grid structure
   outputElement.innerHTML = "<div class='grid-container'></div>";
@@ -37,61 +35,68 @@ function boardLayout() {
   });
 }
 
-let clicks = 0;
-
 function getInput() {
   let gridItems = document.getElementsByClassName("grid-item");
 
   Array.from(gridItems).forEach((item, index) => {
     item.addEventListener("click", function () {
-      if (boardArray[index] === " ") {
-        boardArray[index] = CurrentPlayer;
-        gridItems[index].textContent = CurrentPlayer;
-
-        if (CurrentPlayer === "X") {
-          playerXinputs.push(index);
-        } else {
-          playerOinputs.push(index);
-        }
-
+      if (gameActive && boardArray[index] === " ") { // Check if game is active and grid item is empty
+        boardArray[index] = currentPlayer;
+        gridItems[index].textContent = currentPlayer;
         winConditions();
         Playerswitch();
-        clicks++;
-      } else {
-        checkInput();
       }
     });
   });
 }
 
-const checkInput = () => {
-  let wincon = document.getElementById("winConditions");
-  wincon.innerHTML = "This space is already taken!";
-};
+function Playerswitch() {
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+}
 
 const winConditions = () => {
-  let wincon = document.getElementById("winConditions");
+  const isTie = boardArray.every((item) => item !== " ");
 
-  const isWin = winningCombinations.some((combination) =>
-    combination.every((index) => boardArray[index] === CurrentPlayer)
-  );
+  if (isTie) {
+    alert("It's a tie!");
+    resetBoard();
+  } else {
+    const isWin = winningCombinations.some((combination) =>
+      combination.every((index) => boardArray[index] === currentPlayer)
+    );
 
-  if (isWin) {
-    wincon.innerHTML = `Player ${CurrentPlayer} wins`;
-    game = false;
-  } else if (boardArray.every((item) => item !== " ") && !isWin) {
-    wincon.innerHTML = "It is a tie";
-    game = false;
+    if (isWin) {
+      alert("Player " + currentPlayer + " wins!");
+      if (currentPlayer === "X") {
+        playerXscore++;
+      } else {
+        playerOscore++;
+      }
+      updateScoreboard();
+      gameActive = false; // Turn off the game
+    }
   }
 };
 
-function Playerswitch() {
-  if (CurrentPlayer === "X") {
-    CurrentPlayer = "O";
-  } else {
-    CurrentPlayer = "X";
-  }
+function resetBoard() {
+  boardArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+  boardLayout();
+  getInput();
+  gameActive = true; // Turn on the game
+}
+
+function createScoreboard() {
+  let playerswitch = document.getElementById("playerswitch");
+  playerswitch.innerHTML = "<div class='scoreboard'><span class='score-x'>X - " + playerXscore + "</span><span class='score-separator'>:</span><span class='score-o'>O - " + playerOscore + "</span></div>";
+}
+
+function updateScoreboard() {
+  let scoreX = document.querySelector(".score-x");
+  let scoreO = document.querySelector(".score-o");
+  scoreX.textContent = "X - " + playerXscore;
+  scoreO.textContent = "O - " + playerOscore;
 }
 
 boardLayout();
 getInput();
+createScoreboard();
